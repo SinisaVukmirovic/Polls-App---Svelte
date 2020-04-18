e948d36cacc80428f572a8be08429edee0a2c478
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-04-18 19:33:31 +0200

    Commit before adding problematic Custom Event Dispatcher

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script>
+	// components
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+
+	// tabs
+	import Tabs from './shared/Tabs.svelte';
+
+	// tabs
+	let tabs = ['Current Polls', 'Add New Poll'];
+	let activeTab = 'Current Polls';
 </script>
 
 <Header />
 
 <main>
-	
+	<!-- tabs -->
+	<Tabs {activeTab} {tabs}/>
 </main>
 
 <Footer />
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let tabs;
+    export let activeTab;
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each tabs as tab}
+            <li>
+                <div class:active={tab === activeTab} >{tab}</div>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs {
+        margin-bottom: 2rem;
+    }
+    ul {
+        display: flex;
+        justify-content: center;
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        margin: 0 1rem;
+        font-size: 1rem;
+        color: #ddd;
+        cursor: pointer;
+    }
+    .active {
+        color: coral;
+        border-bottom: 2px solid coral;
+        padding-bottom: .5rem;
+    }
+</style>
