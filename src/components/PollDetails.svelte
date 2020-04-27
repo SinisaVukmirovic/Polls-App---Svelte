39a6524ee28ae98634929d0e1206aa71c65aa0e1
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-04-27 20:11:19 +0200

    Added Vote Bars functionality

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -11,6 +11,9 @@
 
     // reactive values
     $: totalVotes = poll.votesA + poll.votesB;
+    // vote bars
+    $: percentA = Math.floor(100 / totalVotes * poll.votesA);
+    $: percentB = Math.floor(100 / totalVotes * poll.votesB);
 
     // voting functionality
     const handleVote = (option, id) => {
@@ -27,11 +30,11 @@
         <p>Total votes: { totalVotes }</p>
 
         <div class="answer" on:click={()=> handleVote('a', poll.id)}>
-            <div class="percent percent-a"></div>
+            <div class="percent percent-a" style="width: {percentA}%"></div>
             <span>{ poll.answerA } ({ poll.votesA })</span>
         </div>
         <div class="answer" on:click={()=> handleVote('b', poll.id)}>
-            <div class="percent percent-b"></div>
+            <div class="percent percent-b" style="width: {percentB}%"></div>
             <span>{ poll.answerB } ({ poll.votesB })</span>
         </div>
     </div>
@@ -60,4 +63,17 @@
         display: inline-block;
         padding: .5rem 1rem;
     }
+    .percent {
+        height: 100%;
+        position: absolute;
+        box-sizing: border-box;
+    }
+        .percent-a {
+            border-left: 4px solid crimson;
+            background: rgba(237, 20, 61, .3);
+        }
+        .percent-b {
+            border-left: 4px solid lime;
+            background: rgba(0, 255, 0, .3);
+        }
 </style>
